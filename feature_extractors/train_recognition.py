f494756f2bd1124e95dab1f24ff066f16fa2a98d
Matthias Vietz <matthias.vietz@fau.de>
2022-01-11 16:33:57 +0100

    lower learning rate.

--- feature_extractors/train_recognition.py
+++ feature_extractors/train_recognition.py
@@ -287,7 +287,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='Train the CNN-model on images and target ids')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=50, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=32, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.001,
+    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.0001,
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
     # parser.add_argument('--load', '-f', type=str, default="checkpoints/final/eager-wood-82.pth", help='Load model from a .pth file')
